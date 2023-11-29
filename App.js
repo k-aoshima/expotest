a299f24411f0a94ee9b1aaf4fe95ba0714dd3f93
k-aoshima <zakuzaku083@gmail.com>
2023-11-29 22:25:52 +0900

    Add sample bottom tab

--- App.js
+++ App.js
@@ -1,20 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+
+import Top from './components/Top';
+import Contact from "./components/Contact";
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: "#666",
+          },
+          headerTintColor: "#fff",
+          headerTitleStyle: {
+            fontWeight: "bold",
+          },
+          tabBarStyle: {
+            backgroundColor: "#666",
+          },
+          tabBarInactiveTintColor: "#ffffff88",
+          tabBarActiveTintColor: "#fff",
+        }}
+      >
+        <Tab.Screen
+          name="Top"
+          component={Top}
+          options={{
+            title: "TOP",
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons name="home" color={color} size={size} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Contact"
+          component={Contact}
+          options={{
+            title: "お問合わせ",
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons name="information" color={color} size={size} />
+            ),
+          }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
